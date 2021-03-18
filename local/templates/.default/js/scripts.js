6fc2e11a0335a580a79f6758705b55a315d012b4
Miros <lya-job@yandex.ru>
2021-03-18 09:28:38 +0300

    поиск в выпадающих списках

--- local/templates/.default/js/scripts.js
+++ local/templates/.default/js/scripts.js
@@ -46,8 +46,35 @@ $(function(){
 		$(this).parent().toggleClass('active');
 	});
 	/* Select */
+	function matchCustom(params, data) {
+		// If there are no search terms, return all of the data
+		if ($.trim(params.term) === '') {
+			return data;
+		}
+
+		// Do not display the item if there is no 'text' property
+		if (typeof data.text === 'undefined') {
+			return null;
+		}
+
+		// `params.term` should be the term that is used for searching
+		// `data.text` is the text that is displayed for the data object
+		if (data.text.indexOf(params.term) > -1) {
+			var modifiedData = $.extend({}, data, true);
+			modifiedData.text += ' (найдено)';
+
+			// You can return modified objects from here
+			// This includes matching the `children` how you want in nested data sets
+			return modifiedData;
+		}
+
+		// Return `null` if the term should not be displayed
+		return null;
+	}
+
 	$('.js_select').select2({
-		minimumResultsForSearch: -1,
+		//minimumResultsForSearch: -1,
+		matcher: matchCustom,
 		placeholder: "",
 		width: '100%'
 	});
