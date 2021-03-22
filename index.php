ee5b5c23a480a39df1aa118cae7a28cb0803e46f
Alexander Osikov <ALTERoss13@ya.ru>
2021-03-22 08:20:03 +0300

    Исправление ссылок на страницы Настройки и Профиля пользователя

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $APPLICATION->SetTitle("Главная страница");
                         "ID" => $USER->GetID(),
                         "NAME_TEMPLATE" => "#NOBR##NAME# #SECOND_NAME# #LAST_NAME##/NOBR#",
                         "PATH_TO_SONET_USER_PROFILE" => "",
-                        "PROFILE_URL" => "/users/profile/",
+                        "PROFILE_URL" => "/settings/users/my/profile/",
                         "SHOW_FIELDS" => Array("PERSONAL_BIRTHDAY","PERSONAL_ICQ","PERSONAL_PHOTO","PERSONAL_CITY","WORK_COMPANY","WORK_POSITION"),
                         "SHOW_LOGIN" => "Y",
                         "SHOW_YEAR" => "Y",
@@ -37,7 +37,7 @@ $APPLICATION->SetTitle("Главная страница");
             <a href="/clients/" class="tile_link ico_1">Клиенты</a>
             <a href="/insurance-companies/" class="tile_link ico_2">Страховые <br />компании</a>
             <a href="/adjusters/" class="tile_link ico_3">Аджастеры</a>
-            <a href="/html/settings.html" class="tile_link ico_4">Настройки</a>
+            <a href="/settings/" class="tile_link ico_4">Настройки</a>
             <a href="/html/loss_card.html" class="tile_link ico_5">Все убытки</a>
         </div><!-- END tiles_container -->
         <? $APPLICATION->IncludeComponent(
--- settings/index.php
+++ settings/index.php
@@ -10,15 +10,15 @@ $APPLICATION->SetTitle("Персональный раздел");
 				<? $userRoles = new \Itrack\Custom\CUserRole(); ?>
 
 				<?if ($userRoles->isSuperUser()):?>
-					<a href="/settings/users" class="tile_link ico_6">Пользователи</a>
+					<a href="/settings/users/" class="tile_link ico_6">Пользователи</a>
 				<?endif?>
 				<?if ($userRoles->isSuperBroker()):?>
-					<a href="/settings/statuses" class="tile_link ico_7">Статусы</a>
+					<a href="/settings/statuses/" class="tile_link ico_7">Статусы</a>
 				<?endif?>
 				<?if ($userRoles->isSuperBroker() || $userRoles->isSuperClient()):?>
-					<a href="/settings/notifications" class="tile_link ico_8">Уведомления</a>
+					<a href="/settings/notifications/" class="tile_link ico_8">Уведомления</a>
 				<?endif?>
-				<a href="/settings/profile" class="tile_link ico_9">Редактирование профиля</a>
+				<a href="/settings/profile/" class="tile_link ico_9">Редактирование профиля</a>
 			</div><!-- END tiles_container -->
 		</div><!-- END index_content -->
 	</div><!-- END authorization_container -->
