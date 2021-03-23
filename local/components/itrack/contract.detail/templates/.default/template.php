4cf258ce6330509fa64f07d9b513e084dc91b41e
Miros <lya-job@yandex.ru>
2021-03-23 14:49:27 +0300

    добавление убытка - болванка

--- local/components/itrack/contract.detail/templates/.default/template.php
+++ local/components/itrack/contract.detail/templates/.default/template.php
@@ -36,9 +36,164 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
                            placeholder="Поиск по списку убытков"/>
                     <input type="submit" class="search" value=""/>
                 </form><!-- END search_form -->
-                <a href="/html/stats.html" class="btn">Добавить убыток</a>
+                <!-- <a href="/html/stats.html" class="btn">Добавить убыток</a> -->
+                <a href="#add_loss" class="btn" data-fancybox>Добавить убыток</a>
             </div><!-- END title_right_block -->
         </div><!-- END title_container -->
+        <div class="popup" id="add_loss">
+            <h3 class="block_title">Добавление убытка</h3>
+            <form class="form_popup">
+                <div class="form_row">
+                    <div class="input_container column_25">
+                        <input type="text" class="text_input" placeholder="Номер убытка" />
+                    </div><!-- END input_container -->
+                    <div class="input_container column_25">
+                        <input type="text" class="text_input ico_date js_datapicker" placeholder="Дата убытка" />
+                    </div><!-- END input_container -->
+                    <!-- <a href="#" class="link ico_add"><span>Добавить договор страхования</span></a> -->
+                </div><!-- END form_row -->
+                <div class="form_row">
+                    <div class="input_container column_100">
+                        <textarea class="textarea" placeholder="Описание убытка"></textarea>
+                    </div><!-- END input_container -->
+                    <div class="input_container column_50">
+                        <a href="#" class="btn white">Перенести кураторов из договора страхования</a>
+                    </div><!-- END input_container -->
+                </div><!-- END form_row -->
+                <h3 class="subtitle">Кураторы</h3>
+                <h4 class="big_label">Клиент</h4>
+                <div class="gray_block">
+                    <div class="input_container">
+                        <label class="big_label">Компания</label>
+                        <select class="select js_select">
+                            <option>МВМ Entertainment industries</option>
+                            <option>1Untropetion Ltd Companies International</option>
+                            <option>2Untropetion Ltd Companies International</option>
+                            <option>3Untropetion Ltd Companies International</option>
+                            <option>4Untropetion Ltd Companies International</option>
+                        </select><!-- END select -->
+                    </div><!-- END input_container -->
+                </div><!-- END gray_block -->
+                <h4 class="big_label">Страховой брокер</h4>
+                <div class="gray_block">
+                    <div class="input_container">
+                        <label class="big_label">Компания</label>
+                        <select class="select js_select">
+                            <option>МВМ Entertainment industries</option>
+                            <option>1Untropetion Ltd Companies International</option>
+                            <option>2Untropetion Ltd Companies International</option>
+                            <option>3Untropetion Ltd Companies International</option>
+                            <option>4Untropetion Ltd Companies International</option>
+                        </select><!-- END select -->
+                    </div><!-- END input_container -->
+                </div><!-- END gray_block -->
+                <h4 class="big_label">Страховая компания</h4>
+                <div class="gray_block">
+                    <div class="input_container">
+                        <label class="big_label">Компания</label>
+                        <select class="select js_select">
+                            <option>МВМ Entertainment industries</option>
+                            <option>1Untropetion Ltd Companies International</option>
+                            <option>2Untropetion Ltd Companies International</option>
+                            <option>3Untropetion Ltd Companies International</option>
+                            <option>4Untropetion Ltd Companies International</option>
+                        </select><!-- END select -->
+                    </div><!-- END input_container -->
+                </div><!-- END gray_block -->
+                <div class="gray_block">
+                    <div class="input_container">
+                        <label class="big_label">Компания</label>
+                        <select class="select js_select">
+                            <option>МВМ Entertainment industries</option>
+                            <option>1Untropetion Ltd Companies International</option>
+                            <option>2Untropetion Ltd Companies International</option>
+                            <option>3Untropetion Ltd Companies International</option>
+                            <option>4Untropetion Ltd Companies International</option>
+                        </select><!-- END select -->
+                    </div><!-- END input_container -->
+                </div><!-- END gray_block -->
+                <div class="gray_block">
+                    <div class="input_container">
+                        <label class="big_label">Аджастер</label>
+                        <select class="select js_select">
+                            <option>МВМ Entertainment industries</option>
+                            <option>1Untropetion Ltd Companies International</option>
+                            <option>2Untropetion Ltd Companies International</option>
+                            <option>3Untropetion Ltd Companies International</option>
+                            <option>4Untropetion Ltd Companies International</option>
+                        </select><!-- END select -->
+                    </div><!-- END input_container -->
+                </div><!-- END gray_block -->
+                <div class="form_row">
+                    <div class="switches_container">
+                        <label class="big_label">Необходимость акцепта</label>
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Клиент</span>
+                        </div><!-- END switch_container -->
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Страховой Брокер</span>
+                        </div><!-- END switch_container -->
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Страховая Компания</span>
+                        </div><!-- END switch_container -->
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Аджастер</span>
+                        </div><!-- END switch_container -->
+                    </div><!-- END switches_container -->
+                    <div class="switches_container">
+                        <label class="big_label">Уведомления</label>
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Клиент</span>
+                        </div><!-- END switch_container -->
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Страховой Брокер</span>
+                        </div><!-- END switch_container -->
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Страховая Компания</span>
+                        </div><!-- END switch_container -->
+                        <div class="switch_container">
+                            <label class="switch js_checkbox"><input type="checkbox"></label>
+                            <span>Аджастер</span>
+                        </div><!-- END switch_container -->
+                    </div><!-- END switches_container -->
+                </div><!-- END form_row -->
+                <div class="form_row">
+                    <div class="input_container column_100">
+                        <input type="text" class="text_input" placeholder="Запрашиваемые документы" />
+                    </div><!-- END input_container -->
+                </div><!-- END form_row -->
+                <div class="form_row">
+                    <div class="input_container column_25">
+                        <input type="text" class="text_input ico_date js_datapicker" placeholder="Дата запроса" />
+                    </div><!-- END input_container -->
+                    <div class="input_container column_50">
+                        <input type="text" class="text_input" placeholder="Автор запроса" />
+                    </div><!-- END input_container -->
+                    <div class="input_container column_25">
+                        <input type="text" class="text_input ico_date js_datapicker" placeholder="Срок предоставления" />
+                    </div><!-- END input_container -->
+                </div><!-- END form_row -->
+                <div class="form_row">
+                    <label class="big_label">Прикрепить полезные документы</label>
+                    <div class="input_container column_3">
+                        <div class="logo_upload_container without_img">
+                            <div class="logo_upload">
+                                <input type="file" />
+                                <span class="upload"><span>Прикрепить запрос</span></span>
+                            </div><!-- END logo_upload -->
+                        </div><!-- END logo_upload_container -->
+                    </div><!-- END input_container -->
+                </div><!-- END form_row -->
+                <input type="submit" class="btn" value="Добавить договор" />
+            </form><!-- END form_edit_profile -->
+        </div><!-- END popup -->
         <?php if (!empty($arResult['LOSTS'])) : ?>
             <div id="losts-list">
                 <ul class="data_table">
