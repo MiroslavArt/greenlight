51e6a9e3cec6b1acc50b12f22faadce1299efefd
Miros <lya-job@yandex.ru>
2021-04-05 10:53:39 +0300

    запрос - до удаления файла

--- local/components/itrack/lost.request.docs/templates/.default/script.js
+++ local/components/itrack/lost.request.docs/templates/.default/script.js
@@ -1,3 +1,85 @@
 $(document).ready(function() {
+    var inputFile = $('.req_file');
+    var newFiles = [];
+    var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC'
+    };
 
+    var curdate = new Date().toLocaleString("ru", options)
+
+
+    inputFile.change(function(e) {
+       // for(let index = 0; index < e.target.files.length; index++) {
+            var file = $(e.target).prop('files')[0]
+            newFiles.push(file);
+        //}
+        console.log(newFiles)
+
+        var btntxt = BX.findChild(e.target.parentElement, {"class" : "upload_btn_text"}, true, false)
+
+        newFiles.forEach(file => {
+            var fileElement = $(`<p>${file.name}</p>`)
+            $(btntxt).html(fileElement)
+        });
+    })
+
+    $(".upload_doc_container").submit(function (event) {
+        event.preventDefault()
+        var lost_id = $(event.target).attr('data-id')
+        var newcomment = BX.findChild(event.target, {"class" : "text_input"}, true, true)
+        var newcommenttxt = ''
+        newcomment.forEach(function(element){
+            newcommenttxt = $(element).val()
+        })
+        var formData = new FormData()
+        console.log(newFiles[0])
+        formData.append('lost_id', lost_id)
+        formData.append('doc_name', newcommenttxt)
+        formData.append('loss_file', newFiles[0])
+        formData.append('doc_date', curdate)
+        formData.append('comment', newcommenttxt)
+        formData.append('type', '1')
+        $.ajax({
+            url: '/ajax/add_loss_file.php',
+            dataType: 'json',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: formData,
+            type: 'post',
+            success: function(data){
+                console.log(data);
+                if(data.error) {
+                    var mistake = BX.findChild(event.target, {"class" : "mistakereq"}, true, true)
+                    mistake.forEach(function(element){
+                        $(element).text(data.error)
+                    })
+                } else {
+                    //    location.reload();
+                    var liblock = $("<li></li>")
+                    var lidivOne = $("<div></div>").attr('class', 'doc_format')
+                    var lidescOne = $("<div></div>").attr('class', 'doc_list_title').text('Формат документа')
+                    var lispanOne = $("<span></span>").attr('class', 'name_format').text(data.success.type)
+                    lidivOne.append(lidescOne)
+                    lidivOne.append(lispanOne)
+                    liblock.append(lidivOne)
+                    var lidivTwo = $("<div></div>").attr('class', 'doc_desc')
+                    var lidescTwo = $("<div></div>").attr('class', 'doc_list_title').text('Краткое описание')
+                    var lihref = $('<a>').attr('href', data.success.tmp_name).attr('download',data.success.name).text(newcommenttxt)
+                    lidivTwo.append(lidescTwo)
+                    lidivTwo.append(lihref)
+                    liblock.append(lidivTwo)
+                    var delspan = $("<span></span>").attr('class', 'delete js_deletereqdoc').attr('data-id', data.success.docid)
+                    liblock.append(delspan)
+                    console.log(liblock)
+                    var addfile = BX.findChild(event.target.parentElement, {"class" : "doc_list"}, true, false)
+                    console.log(addfile)
+                    $(addfile).append(liblock)
+                }
+            }
+        })
+    })
 });
--- local/components/itrack/lost.request.docs/templates/.default/template.php
+++ local/components/itrack/lost.request.docs/templates/.default/template.php
@@ -8,30 +8,34 @@ use \Bitrix\Main\Localization\Loc;
 ?>
 
 <h2 class="block_title">Запроc</h2>
-<ul class="doc_list">
-<?php foreach ($arResult['DOCUMENTS'] as $arItemdoc) { ?>
-    <li>
-        <div class="doc_format">
-            <p class="doc_list_title">Формат документа</p>
-            <span class="name_format"><?= $arItemdoc['FILE']['CONTENT_TYPE'] ?></span>
-        </div><!-- END doc_format -->
-        <div class="doc_desc">
-            <p class="doc_list_title">Краткое описание</p>
-            <?=(!empty($arItemdoc['FILE']['SRC'])) ? '<a href="' . $arItemdoc['FILE']['SRC'] . '" download>' . $arItemdoc['UF_COMMENT'] . '</a>' : $arItemdoc['UF_COMMENT']?>
-        </div><!-- END doc_format -->
-        <span class="delete"></span>
-    </li>
-<?php } ?>
-<form class="upload_doc_container">
-    <div class="input_container">
-        <input type="text" class="text_input" placeholder="Описание / название документа" />
-    </div><!-- END input_container -->
-    <div class="logo_upload">
-        <input type="file">
-        <span class="upload">
+<div class="doc_conti">
+    <ul class="doc_list">
+        <?php foreach ($arResult['DOCUMENTS'] as $arItemdoc) { ?>
+            <li>
+                <div class="doc_format">
+                    <p class="doc_list_title">Формат документа</p>
+                    <span class="name_format"><?= $arItemdoc['FILE']['CONTENT_TYPE'] ?></span>
+                </div><!-- END doc_format -->
+                <div class="doc_desc">
+                    <p class="doc_list_title">Краткое описание</p>
+                    <?=(!empty($arItemdoc['FILE']['SRC'])) ? '<a href="' . $arItemdoc['FILE']['SRC'] . '" download>' . $arItemdoc['UF_COMMENT'] . '</a>' : $arItemdoc['UF_COMMENT']?>
+                </div><!-- END doc_format -->
+                <span class="delete js_deletereqdoc" data-id="<?=$arItemdoc['ID']?>"></span>
+            </li>
+        <?php } ?>
+    </ul>
+    <form class="upload_doc_container" data-id="<?=$arParams['LOST_DOC']?>">
+        <div class="input_container">
+            <input type="text" class="text_input" placeholder="Описание / название документа" />
+        </div><!-- END input_container -->
+        <div class="logo_upload">
+            <input type="file" class="req_file">
+            <span class="upload">
             <span>Файл</span>
             <span class="upload_btn_text">Загрузите <br />пожалуйста файл</span>
         </span><!-- END upload -->
-    </div><!-- END logo_upload -->
-    <button type="submit" class="btn">Добавить <br />документ</button>
-</form><!-- END upload_doc_container -->
+        </div><!-- END logo_upload -->
+        <p class="link mistakereq"></p>
+        <button type="submit" class="btn">Добавить <br />документ</button>
+    </form><!-- END upload_doc_container -->
+</div>
