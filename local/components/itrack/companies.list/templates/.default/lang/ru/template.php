4eed5db356886e11190f9ea175f18a9084988fa5
Miros <lya-job@yandex.ru>
2021-04-20 12:43:35 +0300

    стили и метки

--- local/components/itrack/companies.list/templates/.default/lang/ru/template.php
+++ local/components/itrack/companies.list/templates/.default/lang/ru/template.php
@@ -3,6 +3,6 @@ $MESS['add_client'] = 'Добавить клиента';
 $MESS['add_insurer'] = 'Добавить страховую компанию';
 $MESS['add_adjuster'] = 'Добавить аджастера';
 
-$MESS['search_client'] = 'Поиск по списку клиентов';
+$MESS['search_client'] = 'Поиск клиента';
 $MESS['search_insurer'] = 'Поиск страховой компании';
 $MESS['search_adjuster'] = 'Поиск аджастера';
