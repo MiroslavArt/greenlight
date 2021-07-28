06dbfcf44ce020fbb66ae6359b19f47949603154
Miros <lya-job@yandex.ru>
2021-07-28 10:03:52 +0300

    логотипы СК

--- local/components/itrack/contract.detail/templates/.default/include/insurance_companies_popup.php
+++ local/components/itrack/contract.detail/templates/.default/include/insurance_companies_popup.php
@@ -6,7 +6,7 @@
     <?php foreach ($arResult['INSURANCE_COMPANIES'] as $arItem) : ?>
     <div class="client_block">
         <?php if(!empty($arItem['PROPERTIES']['LOGO']['VALUE']) && intval($arItem['PROPERTIES']['LOGO']['VALUE']) > 0 ) :?>
-        <img src="<?=CFile::ResizeImageGet($arResult['INSURANCE_COMPANY']['PROPERTIES']['LOGO']['VALUE'], ['width' => 40, 'height' => 40])['src']?>" alt="<?=$arItem['NAME']?>" width="40" height="40">
+        <img src="<?=CFile::ResizeImageGet($arItem['PROPERTIES']['LOGO']['VALUE'], ['width' => 40, 'height' => 40])['src']?>" alt="<?=$arItem['NAME']?>" width="40" height="40">
         <?php endif; ?>
         <span><?=$arItem['NAME']?></span>
     </div>
